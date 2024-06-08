704ac717cae58630011e27710ea56e07e028e580
Kabir4874 <kabir.cse.bd@gmail.com>
2024-06-08 21:25:01 +0600

    add

--- dashboard/src/router/routes/adminRoutes.js
+++ dashboard/src/router/routes/adminRoutes.js
@@ -1,10 +1,8 @@
 import { lazy } from "react";
-
 const AdminDashboard = lazy(() => import("../../views/admin/AdminDashboard"));
 const Orders = lazy(() => import("../../views/admin/Orders"));
 const Category = lazy(() => import("../../views/admin/Category"));
 const Sellers = lazy(() => import("../../views/admin/Sellers"));
-
 export const adminRoutes = [
   {
     path: "/admin/dashboard",
