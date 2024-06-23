00b8508137cecd2bea9fe2c57ad6a3de2e00594f
Kabir4874 <kabir.cse.bd@gmail.com>
2024-06-23 18:05:00 +0600

    add

--- dashboard/src/views/admin/SellerDetails.jsx
+++ dashboard/src/views/admin/SellerDetails.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const SellerDetails = () => {
   return (
-    <div>SellerDetails</div>
-  )
-}
+    <div className="px-2 lg:px-7 pt-5">
+      <div className="w-full p-4 bg-ebony_clay rounded-md">
+        <div className="w-full flex flex-wrap text-iron">
+          <div className="w-3/12 flex justify-center items-center py-3">
+            <div>
+              <img
+                src="http://localhost:3000/images/admin.jpg"
+                alt=""
+                className="w-full h-[230px]"
+              />
+            </div>
+          </div>
+          <div className="w-4/12">
+          <div>
+            
+          </div>
 
-export default SellerDetails
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SellerDetails;
