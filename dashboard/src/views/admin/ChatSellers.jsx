f370c1a7f45827a7210cb93f2e9608afe8751cac
Kabir4874 <kabir.cse.bd@gmail.com>
2024-06-24 17:24:52 +0600

    add

--- dashboard/src/views/admin/ChatSellers.jsx
+++ dashboard/src/views/admin/ChatSellers.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { IoMdClose } from "react-icons/io";
+import { FaList } from "react-icons/fa";
 
 const ChatSellers = () => {
   const [show, setShow] = useState(false);
+  const sellerId = 32;
   return (
     <div className="px-2 lg:px-7 py-5">
       <div className="w-full bg-ebony_clay px-4 py-4 rounded-md h-[calc(100vh-140px)]">
@@ -17,13 +19,13 @@ const ChatSellers = () => {
                 <h2>Sellers</h2>
                 <span
                   className=" block cursor-pointer md:hidden"
-                  onClick={() => setShow(true)}
+                  onClick={() => setShow(!show)}
                 >
                   <IoMdClose />
                 </span>
               </div>
               <div
-                className={`h-[60px] flex justify-start gap-2 items-center text-white px-2 py-2 rounded-sm cursor-pointer`}
+                className={`h-[60px] flex justify-start gap-2 items-center text-white px-2 py-2 rounded-sm cursor-pointer bg-slate-700`}
               >
                 <div className="relative">
                   <img
@@ -75,10 +77,63 @@ const ChatSellers = () => {
               </div>
             </div>
           </div>
+
+          <div className="w-full md:w-[calc(100%-200px)] md:pl-4">
+            <div className="flex justify-between items-center">
+              {sellerId && (
+                <div className="flex justify-start items-center gap-3">
+                  <div className="relative">
+                    <img
+                      src="http://localhost:3000/images/admin.jpg"
+                      alt=""
+                      className="w-[42px] h-[42px] border-green-500 border-2 max-w-[42px] p-[2px] rounded-full"
+                    />
+                    <div className="w-[10px] h-[10px] bg-green-500 rounded-full absolute right-0 bottom-0"></div>
+                  </div>
+                </div>
+              )}
+              <div
+                onClick={() => setShow(!show)}
+                className="w-[35px] flex md:hidden h-[35px] rounded-sm bg-blue-500 shadow-lg hover:shadow-blue-500/50 justify-center cursor-pointer items-center text-white"
+              >
+                <span>
+                  <FaList />
+                </span>
+              </div>
+            </div>
+
+            <div className="py-4">
+              <div className="bg-slate-800 h-[calc(100vh-290px)] rounded-md p-3 overflow-y-auto">
+                <div className="w-full flex justify-start items-center">
+                  <div className="flex justify-start items-start gap-2 md:px-3 py-2 max-w-full lg:max-w-[85%]">
+                    <div>
+                      <img
+                        src="http://localhost:3000/images/admin.jpg"
+                        alt=""
+                        className="w-[38px] h-[38px] border-white border-2 max-w-[38px] p-[2px] rounded-full"
+                      />
+                      <div className="w-[10px] h-[10px] bg-green-500 rounded-full absolute right-0 bottom-0"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <form className="flex gap-3">
+              <input
+                className="w-full flex justify-between px-2 border border-slate-700 items-center py-[5px] focus:border-blue-500 rounded-md outline-none bg-transparent text-iron"
+                type="text"
+                placeholder="input your message"
+              />
+              <button className=" bg-cyan-500 shadow-lg hover:shadow-cyan-500/50 font-semibold w-[75px] h-[35px] rounded-md text-white flex justify-center items-center">
+                Send
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default ChatSellers; 
+export default ChatSellers;
