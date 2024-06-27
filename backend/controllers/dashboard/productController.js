e0ddb5b82947b70ec8a96dfff4244b8bfeaeb0f3
Kabir4874 <kabir.cse.bd@gmail.com>
2024-06-27 15:42:55 +0600

    add

--- backend/controllers/dashboard/productController.js
+++ backend/controllers/dashboard/productController.js
@@ -1,6 +1,20 @@
+const { formidable } = require("formidable");
+const { responseReturn } = require("../../utils/response");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+  secure: true,
+});
+
 class productController {
   add_product = async (req, res) => {
-    console.log("add product");
+    const form = formidable({ multiples: true });
+    form.parse(req, async (error, fields, files) => {
+      console.log(files, fields);
+    });
   };
   get_product = async (req, res) => {};
 }
