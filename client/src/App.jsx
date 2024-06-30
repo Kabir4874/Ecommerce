266d6135395848719f08465bd69f49a22021669f
Kabir4874 <kabir.cse.bd@gmail.com>
2024-06-30 22:20:16 +0600

    add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,15 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full bg-white'>
+        <div className='header-top bg-gallery md-lg:hidden'>
+            sal;dfkj sadkf
+        </div>
+        
+    </div>
+  )
+}
+
+export default Header
