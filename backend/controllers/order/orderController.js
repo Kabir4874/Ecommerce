0fce7b5e6f11bae497f8cd2642084471105ff580
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2024-07-25 09:49:58 +0600

    add

--- backend/controllers/order/orderController.js
+++ backend/controllers/order/orderController.js
@@ -1,6 +1,49 @@
-class orderController{
-    place_order= async(req,res)=>{
-        console.log(req.body);
+const authorModel = require("../../models/authOrderModel");
+const customerOrderModel = require("../../models/customerOrderModel");
+const moment = require("moment");
+const { responseReturn } = require("../../utils/response");
+class orderController {
+  place_order = async (req, res) => {
+    const { price, products, shipping_fee, shippingInfo, userId } = req.body;
+    let authorOrderData = [];
+    let cardId = [];
+    const tempDate = moment(Date.now()).format("LLL");
+    let customerORderProduct = [];
+    for (let i = 0; i < products.length; i++) {
+      const pro = products[i].products;
+      for (let j = 0; j < pro.length; j++) {
+        let tempCuspro = pro[j].productInfo;
+        customerORderProduct.push(tempCuspro);
+        if (pro[j]._id) {
+          cardId.push(pro[j]._id);
+        }
+      }
     }
+    try {
+      const order = await customerOrderModel.create({
+        customerId: userId,
+        shippingInfo,
+        products: customerORderProduct,
+        price: price + shipping_fee,
+        delivery_status: "pending",
+        payment_status: "unpaid",
+        date: tempDate,
+      });
+      for (let i = 0; i < products.length; i++) {
+        const pro = products[i].products;
+        const pri = products[i].price;
+        const sellerId = products[i].sellerId;
+        let storePro = [];
+        for (let j = 0; j < pro.length; j++) {
+          let tempPro = pro[j].productInfo;
+          tempPro.quantity = pro[j].quantity;
+          storePro.push(tempPro);
+        }
+      }
+      responseReturn(res, 201, { message: "Order Placed Successfully" });
+    } catch (error) {
+      responseReturn(res, 501, { error: error.message });
+    }
+  };
 }
-module.exports= new orderController()
+module.exports = new orderController();
