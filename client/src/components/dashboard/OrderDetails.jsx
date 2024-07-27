d9d2241b26327ae26a5b79a825ded32670dd6449
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2024-07-27 22:51:26 +0600

    add

--- client/src/components/dashboard/OrderDetails.jsx
+++ client/src/components/dashboard/OrderDetails.jsx
@@ -1,19 +1,20 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { get_order_details } from "../../store/reducers/orderReducer";
 
 const OrderDetails = () => {
-    const {orderId}= useParams();
-    const dispatch= useDispatch();
-    const {myOrder}= useSelector(state=>state.order);
-    useEffect(()=>{
-        dispatch()
-    },[])
+  const { orderId } = useParams();
+  const dispatch = useDispatch();
+  const { myOrder } = useSelector((state) => state.order);
+  useEffect(() => {
+    dispatch(get_order_details(orderId));
+  }, []);
   return (
-    <div className='bg-white p-5'>
-
+    <div className="bg-white p-5">
+      <h2></h2>
     </div>
-  )
-}
+  );
+};
 
-export default OrderDetails
+export default OrderDetails;
